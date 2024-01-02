ceeac57ac57bbd93105415e46ec5f2fe95c7141d
John Sanpe <sanpeqf@gmail.com>
2024-01-02 08:33:17 +0800

    feat commands: added initial commands
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- src/commands.c
+++ src/commands.c
@@ -3,5 +3,142 @@
  * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
  */
 
-#include <lksu/kernel.h>
 #include <command.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/prctl.h>
+#include <lksu/kernel.h>
+
+static inline int
+lksu_syscall(struct lksu_message *msg)
+{
+    return prctl(LKSU_SYSCALL_CTLKEY, (uintptr_t)msg);
+}
+
+DEFINE_COMMAND(enable)
+{
+    struct lksu_message msg;
+
+    if (argc != 2) {
+        printf("Usage: enable token\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_ENABLE;
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+
+}
+
+DEFINE_COMMAND(disable)
+{
+    struct lksu_message msg;
+
+    if (argc != 2) {
+        printf("Usage: disable token\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_DISABLE;
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
+
+DEFINE_COMMAND(g_hidden_add)
+{
+    struct lksu_message msg;
+
+    if (argc != 3) {
+        printf("Usage: g_hidden_add token path\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_GLOBAL_HIDDEN_ADD;
+    msg.args.g_hidden = argv[2];
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
+
+DEFINE_COMMAND(g_hidden_remove)
+{
+    struct lksu_message msg;
+
+    if (argc != 3) {
+        printf("Usage: g_hidden_remove token path\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_GLOBAL_HIDDEN_REMOVE;
+    msg.args.g_hidden = argv[2];
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
+
+DEFINE_COMMAND(g_uid_add)
+{
+    struct lksu_message msg;
+
+    if (argc != 3) {
+        printf("Usage: g_uid_add token uid\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_GLOBAL_UID_ADD;
+    msg.args.g_uid = atoi(argv[2]);
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
+
+DEFINE_COMMAND(g_uid_remove)
+{
+    struct lksu_message msg;
+
+    if (argc != 3) {
+        printf("Usage: g_uid_remove token uid\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_GLOBAL_UID_REMOVE;
+    msg.args.g_uid = atoi(argv[2]);
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
+
+DEFINE_COMMAND(token_add)
+{
+    struct lksu_message msg;
+
+    if (argc != 3) {
+        printf("Usage: token_add token token_to_add\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_TOKEN_ADD;
+    strncpy(msg.args.token, argv[2], LKSU_TOKEN_LEN);
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
+
+DEFINE_COMMAND(token_remove)
+{
+    struct lksu_message msg;
+
+    if (argc != 3) {
+        printf("Usage: token_remove token token_to_remove\n");
+        return -EINVAL;
+    }
+
+    msg.func = LKSU_TOKEN_REMOVE;
+    strncpy(msg.args.token, argv[2], LKSU_TOKEN_LEN);
+    strncpy(msg.token, argv[1], LKSU_TOKEN_LEN);
+
+    return lksu_syscall(&msg);
+}
